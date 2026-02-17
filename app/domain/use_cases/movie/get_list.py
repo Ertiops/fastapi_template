8a649f4a6d0f9297e83fba0343e6a25486d3d9fd
Elijah Tomin <104155685+Ertiops@users.noreply.github.com>
2026-02-17 10:52:38 +0300

    added ItemList entity (#11)
    Co-authored-by: Eljan <eljan@MacBook-Air-Eljan.local>

--- app/domain/use_cases/movie/get_list.py
+++ app/domain/use_cases/movie/get_list.py
@@ -1,10 +1,11 @@
 from app.application.use_case import IUseCase
-from app.domain.entities.movie import MovieList, MovieListParams
+from app.domain.entities.common import ItemList
+from app.domain.entities.movie import Movie, MovieListParams
 from app.domain.interfaces.storages.movie import IMovieStorage
 from app.domain.uow import AbstractUow
 
 
-class GetMovieListUC(IUseCase[MovieListParams, MovieList]):
+class GetMovieListUC(IUseCase[MovieListParams, ItemList[Movie]]):
     _movie_storage: IMovieStorage
     _uow: AbstractUow
 
@@ -16,8 +17,8 @@ class GetMovieListUC(IUseCase[MovieListParams, MovieList]):
         self._movie_storage = movie_storage
         self._uow = uow
 
-    async def execute(self, *, input_dto: MovieListParams) -> MovieList:
+    async def execute(self, *, input_dto: MovieListParams) -> ItemList[Movie]:
         async with self._uow:
             total = await self._movie_storage.count(input_dto=input_dto)
             items = await self._movie_storage.get_list(input_dto=input_dto)
-        return MovieList(total=total, items=items)
+        return ItemList(total=total, items=items)
--- tests/domain/use_cases/user/test_get_list.py
+++ tests/domain/use_cases/user/test_get_list.py
@@ -1,9 +1,9 @@
 from collections.abc import Awaitable, Callable
 
 from app.adapters.database.tables import UserTable
+from app.domain.entities.common import ItemList
 from app.domain.entities.user import (
     User,
-    UserList,
     UserListParams,
 )
 from app.domain.use_cases.user.get_list import GetUserListUC
@@ -15,7 +15,7 @@ async def test__get_list(
 ) -> None:
     db_users = [await create_user() for _ in range(2)]
     users = await get_user_list_uc.execute(input_dto=UserListParams(limit=10, offset=0))
-    assert users == UserList(
+    assert users == ItemList(
         total=len(db_users),
         items=[
             User(
@@ -36,7 +36,7 @@ async def test__get_list__validate_limit(
 ) -> None:
     db_users = [await create_user() for _ in range(2)]
     users = await get_user_list_uc.execute(input_dto=UserListParams(limit=1, offset=0))
-    assert users == UserList(
+    assert users == ItemList(
         total=len(db_users),
         items=[
             User(
@@ -57,7 +57,7 @@ async def test__get_list__validate_offset(
 ) -> None:
     db_users = [await create_user() for _ in range(2)]
     users = await get_user_list_uc.execute(input_dto=UserListParams(limit=1, offset=1))
-    assert users == UserList(
+    assert users == ItemList(
         total=len(db_users),
         items=[
             User(
@@ -76,4 +76,4 @@ async def test__get_list__empty_list(
     get_user_list_uc: GetUserListUC,
 ) -> None:
     users = await get_user_list_uc.execute(input_dto=UserListParams(limit=10, offset=0))
-    assert users == UserList(total=0, items=[])
+    assert users == ItemList(total=0, items=[])
